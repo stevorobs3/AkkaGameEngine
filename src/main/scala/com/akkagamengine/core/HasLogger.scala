f0263bbee39a9cdc9dd2caa7031126ddf35ef356
Steve <steve.robinson290391@gmail.com>
2017-03-02 21:45:08 +0000

    Add logger

--- src/main/scala/com/akkagamengine/core/HasLogger.scala
+++ src/main/scala/com/akkagamengine/core/HasLogger.scala
@@ -1,9 +1,19 @@
 package com.akkagamengine.core
 
+import com.badlogic.gdx.Gdx
+
 trait HasLogger {
   val Prefix : String
 
-  def info(message : String) : Unit = {
+  def debug(message : String) : Unit = {
+    Gdx.app.debug(Prefix, message)
+  }
 
+  def info(message : String) : Unit = {
+    Gdx.app.log(Prefix, message)
+  }
+
+  def error(message : String) : Unit = {
+    Gdx.app.debug(Prefix, message)
   }
 }
