a1a94338b6a81d764ed3738ddbab883d034babed
Steve <steve.robinson290391@gmail.com>
2017-03-02 22:24:02 +0000

    Draw a simple image

--- src/main/scala/com/akkagamengine/core/AkkaGameEngine.scala
+++ src/main/scala/com/akkagamengine/core/AkkaGameEngine.scala
@@ -1,11 +1,16 @@
 package com.akkagamengine.core
 
-import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.g2d.{Sprite, SpriteBatch}
+import com.badlogic.gdx.graphics.{GL20, Texture}
 import com.badlogic.gdx.{ApplicationListener, Gdx}
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
-class AkkaGameEngine extends ApplicationListener {
-  lazy val spriteBatch = new SpriteBatch()
+class AkkaGameEngine extends ApplicationListener
+  with HasLogger {
+  val LogId = "AkkaGameEngine"
+
+  lazy val batch = new SpriteBatch()
+  lazy val texture = new Texture(Gdx.files.internal("Assets/libgdx.png"))
+  var sprite : Sprite = new Sprite()
 
   override def create(): Unit = {
     Gdx.gl.glClearColor(1, 0, 0, 1)
@@ -13,16 +18,19 @@ class AkkaGameEngine extends ApplicationListener {
 
   override def render(): Unit = {
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+    batch.begin()
+    batch.draw(texture,0,0)
+    batch.end()
   }
-
-  private def update(func: SpriteBatch => Unit) = {
-  }
-  override def dispose(): Unit = {
-  }
-
-  override def resume(): Unit = {}
 
   override def resize(width: Int, height: Int): Unit = {}
 
+  override def dispose(): Unit = {
+    batch.dispose()
+    texture.dispose()
+  }
+
   override def pause(): Unit = {}
+
+  override def resume(): Unit = {}
 }
